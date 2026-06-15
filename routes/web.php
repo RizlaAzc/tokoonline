0e160a8267ce3f76f65c0e829ab60173315d46e9
RizlaAzc <azchafahrezi@gmail.com>
2026-06-15 21:59:22 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BerandaController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\ProdukController;
+Route::get('/', function () {
+// return view('welcome');
+return redirect()->route('beranda');
